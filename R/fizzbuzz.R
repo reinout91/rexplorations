b9a4930b9b25e9c4405ae82a37af2079c9f358ae
Reinout Holtrup <al22954@lp0148.alliander.local>
2021-06-10 16:40:35 +0200

    initial commit & fizzbuzz

---
+++ R/fizzbuzz.R
@@ -0,0 +1,8 @@
+library(lambda.r)
+fizz(n) %::% numeric : character
+fizz(n) %when% {15 %% n == 0} %as% {"fizzbuzz"}
+fizz(n) %when% {n  %% 3  == 0}  %as% {"fizz"}
+fizz(n) %when% {n  %% 5  == 0}  %as% {"buzz"}
+fizz(n) %as% {as.character(n)}
+
+print(sapply(1:100, fizz))
