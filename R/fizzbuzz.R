1669904b833495619a02a270f9ce78a842cd3a45
Reinout Holtrup <al22954@lp0148.alliander.local>
2021-06-10 16:48:59 +0200

    fix

--- R/fizzbuzz.R
+++ R/fizzbuzz.R
@@ -1,6 +1,6 @@
 library(lambda.r)
 fizz(n) %::% numeric : character
-fizz(n) %when% {15 %% n == 0} %as% {"fizzbuzz"}
+fizz(n) %when% {n %% 15 == 0} %as% {"fizzbuzz"}
 fizz(n) %when% {n  %% 3  == 0}  %as% {"fizz"}
 fizz(n) %when% {n  %% 5  == 0}  %as% {"buzz"}
 fizz(n) %as% {as.character(n)}
