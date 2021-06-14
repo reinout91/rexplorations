ae77096d2e2a0343cc1b6f858035dcb25a979c5f
Reinout Holtrup <reinout91@gmail.com>
2021-06-14 16:59:30 +0200

    Update test-belastingprognose_RSelenium.R

--- R/test-belastingprognose_RSelenium.R
+++ R/test-belastingprognose_RSelenium.R
@@ -5,19 +5,19 @@ rD <- rsDriver(browser = "chrome", port = 4547L, verbose = FALSE, chromever = "9
 remDr <- rD[["client"]]
 
 remDr$navigate("https://shiny-analytics.alliander.local/belastingprognosedashboard")
-Sys.sleep(5)
+Sys.sleep(2)
 remDr$findElement(using = 'xpath', value = r"{//*[@id="shiny-modal"]/div/div/div[3]/button}")$clickElement()
-Sys.sleep(5)
+Sys.sleep(2)
 remDr$findElement(using = 'xpath', value = r"{//*[@id="AC5kaart-open_ms_prognose_dashboard"]}")$clickElement()
-Sys.sleep(5)
+Sys.sleep(2)
 remDr$findElement(using = 'xpath', value = r"{//*[@id="AC5kaart-ms_prognose_dashboard"]/div/div/div[1]/div[1]/div[1]/div/div/div/div[1]}")$clickElement()
-Sys.sleep(5)
+Sys.sleep(2)
 remDr$findElement(using = 'xpath', value  = r"{//div/div/div/div[2]/div/div[5]}")$clickElement() #selecteer de 5e optie uit de lijst.
-Sys.sleep(5)
+Sys.sleep(2)
 remDr$findElement(using = 'xpath', value  = r"{/html/body/div[1]/div[2]/div[7]/div/div/div[1]/div[3]/ul/li[7]/a}")$clickElement()
-Sys.sleep(5)
+Sys.sleep(2)
 remDr$findElement(using = 'xpath', value  = r"{//*[@id="belastingprognose-klantinpassing-inladen_data_installatie"]}")$clickElement()
-Sys.sleep(5)
+Sys.sleep(2)
 
 remDr$close()
 rm(rD, remDr)
