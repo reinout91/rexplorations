04e267382d5c5120339ece0d8e355735d61b12ab
Reinout Holtrup <reinout91@gmail.com>
2021-12-23 16:47:51 +0100

    Create fourier_analyse.R

---
+++ R/fourier_analyse.R
@@ -0,0 +1,181 @@
+#' @title aki_hackathon_sta
+#' @description Season trend analyse van profieldata
+#' @import prophet
+#' @import data.table
+#' @import plotly
+aki_hackathon_sta <- function(){
+  # profiel_data <- readRDS("/data/workspace/AKI_hackathon_sta/data_alkmaar.RDS")
+  profiel_data <- readRDS(system.file('rds/data_alkmaar.RDS', package = packageName()))
+
+  # forecast_data <- unique(forecast_data#[year < 2022, .(ds, y)])
+  
+  profiel_data$installatie_profiel$DATUM_TIJD <- paste0(profiel_data$installatie_profiel$DATUM_TIJD, ":00")
+  
+  forecast_data <- profiel_data$installatie_profiel[JAAR %in% c(2020:2025), .(ds = DATUM_TIJD, y = BELASTING)]
+  
+  model <- prophet(forecast_data)
+  future <- make_future_dataframe(model, periods=60*60)
+  forecast <- predict(model, future)
+  
+  ggplotly(plot(model, forecast))
+  
+  
+  prophet_plot_components(model, forecast)
+  
+  ggplotly(prophet_plot_components(model, forecast)[[1]])
+  
+  return(1)
+}
+
+
+#' @title aki_hackathon_sta2
+#' @description Season trend analyse van profieldata2
+#' @import fpp3
+#' @import data.table
+#' @import slider
+#' @import spectral
+#' @import signal
+#' @import control
+#' @import plotly
+#' @import patchwork
+aki_hackathon_sta2 <- function(){
+  trucje<- packageName()
+  if (is.null(trucje)) {trucje <- 'PROJECTNAME'}
+  
+  # profiel_data <- readRDS("/data/workspace/AKI_hackathon_sta/data_alkmaar.RDS")
+  profiel_data <- readRDS(system.file('rds/data_alkmaar.RDS', package = trucje))
+  
+  # forecast_data <- unique(forecast_data#[year < 2022, .(ds, y)])
+  
+  profiel_data$installatie_profiel$DATUM_TIJD <- paste0(profiel_data$installatie_profiel$DATUM_TIJD, ":00")
+  forecast_data <- profiel_data$installatie_profiel[JAAR %in% c(2020:2025), .(ds = lubridate::as_datetime(DATUM_TIJD), y = BELASTING)]
+  schrikkeldag <-forecast_data[ds < as.Date('2020-02-23') & ds >= as.Date('2020-02-22')][, ds:= ds+ days(7)]
+  forecast_data <- rbind(forecast_data, schrikkeldag)
+  
+  #teken profiel
+  series <- tsibble(forecast_data, index=ds)
+  series %>%
+    autoplot(y, colour="gray") %>% ggplotly()
+  # --> zomermaanden kleiner verschil tussen dag/nacht dan in de winter.
+  # --> dat maakt het wat lastig te vangen in een puur additief model.
+  
+  
+  #Fourier analyse
+  ft <- stats::fft(series$y)
+  t_s <- 15 * 60 #[sec] 
+  
+  freq <- (1:length(ft)) * t_s / length(ft)
+  A <- sqrt(Re(ft)^2 + Im(ft)^2) # length of the number
+  PSD <- data.frame(freq, A)
+  ggplotly(ggplot(PSD, aes(x = freq, y = A)) + geom_line() 
+           + xlab("frequency Rad/s")
+          + ylab("spectral density"))
+  
+  #TODO Dit Power density plot interpreteren.
+  
+  # Filter ontwerp in R, daarvoor kun je de package control gebruiken.
+  # Eventueel zou je ook nog kunnen kijken naar sysid, al betreft dat meer functies voor het interpreteren van step en frequency responses.
+  signal <- as.matrix(series$y)
+  
+  num <- c(1)
+  den <- c(0.5, 1)
+  filter <- control::tf(num, den) #TODO Deze nog ontwerpen
+  bodeplot(tf2ss(num, den))
+  
+  resp_bode <- bode(filter, seq(0, 100, length = 10000))
+  
+     
+  bode <-  ggplot(data.frame(resp_bode), aes(x = w)) +
+    geom_line() + scale_x_log10()
+  
+  bode_gain <- bode + aes(y = mag) +
+    xlab("Frequency [rad/sec]") +
+    ylab("Magnitude [dB]") + 
+    labs(title = "Bode Magnitude")+
+    theme_minimal()
+  
+  bode_phase <- bode + aes(y = phase) +
+    xlab("Frequency [rad/sec]") +
+    ylab("Phase [deg]") +
+    labs(title = "Bode Phase") + theme_minimal()
+  
+  bode_gain + bode_phase + plot_layout(ncol = 1)
+  
+  bodeplot <- plotly::subplot(bode_gain, bode_phase, nrows = 2, shareX = TRUE, shareY = FALSE, titleY = TRUE, titleX = TRUE)
+  
+  control::lsimplot(sys = filter, u = signal, t = series$ds)
+  resp = lsim(sys = filter, u = -signal, t = series$ds)
+  mmm <- data.frame(t=resp$t, filtered = resp$y[1,], original = series$y)
+  (ggplot(mmm) + 
+    geom_line(aes(x=t,y=original), colour = "gray") +
+    geom_line(aes(x=t,y=-filtered), colour = "red")) %>% ggplotly
+    
+  
+  #TODO: Dit idee verder uitwerken, filteren per tijdblok, bijvoorbeeld dagen etc.
+  #Zelf dus kiezen welke tijdsperiodes nuttig zijn.
+  
+  #TODO;
+  #Arima modellen zijn de gegeneraliseerde vorm voorfrequentiedecompositie.
+  #Er bestaat ook SARIMA, Seasonal Arima, waarin specifieke frequentiebereuken voor dagen, weken, maanden etc zijn opgenomen.
+  #TODO Test of SARIMA ook nog nuttig kan zijn.
+  
+  
+  #Volledige decompositie
+  dcmp <- series %>%
+    model(stl = STL(y))
+  
+  decomp <- components(dcmp)
+  
+  decomp %>%
+    autoplot(decomp$y, colour="gray") +
+    geom_line(aes(x=decomp$ds, y=decomp$trend), colour = "#D55E00") +
+    labs(
+      y = "Vermogen KVA",
+      title = "Vermogen KVA"
+    )
+  
+  components(dcmp) %>% autoplot() %>% ggplotly()
+  
+  #Plotje van het trenddeel van de decompositie
+  plt <- components(dcmp) %>%
+    as_tsibble() %>%
+    autoplot(y, colour="gray") +
+    geom_line(aes(y=trend), colour = "#D55E00") +
+    labs(
+      y = "Vermogen KVA",
+      title = "Vermogen KVA - trend"
+    ) 
+  plt %>% ggplotly
+  
+  
+  #Moving average van 1 dag
+  series <- series %>% mutate(m_avg = series %>% {slider::slide_dbl(.$y, mean,
+                    .before = 4*12, .after = 4*12, .complete = TRUE)})
+  
+  plt<- series %>%
+    autoplot(y, colour="gray") +
+    geom_line(aes(y = `m_avg`), colour = "#D55E00") +
+    labs(y = "% of GDP",
+         title = "Vermogen KVA - moving average 1 dag") +
+    guides(colour = guide_legend(title = "series")) 
+  ggplotly(plt)
+  #TODO: Stop deze in een high pass filter en bepaal de amplitude.
+  
+
+  
+  #Prophet
+  forecast_data <- profiel_data$installatie_profiel[JAAR %in% c(2020:2025), .(ds = DATUM_TIJD, y = BELASTING)]
+
+  model <- prophet(forecast_data)
+  future <- make_future_dataframe(model, periods=1)
+  forecast <- predict(model, future)
+
+  ggplotly(plot(model, forecast))
+
+
+  prophet_plot_components(model, forecast)
+
+  ggplotly(prophet_plot_components(model, forecast)[[1]])
+  
+  return(1)
+}
