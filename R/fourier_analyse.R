2dce295dcc8f02346731abb72c91d0849b112f66
Reinout Holtrup <reinout91@gmail.com>
2021-12-24 15:42:29 +0100

    blah

--- R/fourier_analyse.R
+++ R/fourier_analyse.R
@@ -34,7 +34,6 @@ aki_hackathon_sta <- function(){
 #' @import data.table
 #' @import slider
 #' @import spectral
-#' @import signal
 #' @import control
 #' @import plotly
 #' @import patchwork
@@ -67,15 +66,36 @@ aki_hackathon_sta2 <- function(){
   freq <- (1:length(ft)) * t_s / length(ft)
   A <- sqrt(Re(ft)^2 + Im(ft)^2) # length of the number
   PSD <- data.frame(freq, A)
-  ggplotly(ggplot(PSD, aes(x = freq, y = A)) + geom_line() 
-           + xlab("frequency Rad/s")
-          + ylab("spectral density"))
+  ggplotly(ggplot(PSD, aes(x = freq, y = A)) + geom_line() +
+             xlab("frequency rad/s") +
+             ylab("spectral density"))
   
   #TODO Dit Power density plot interpreteren.
   
   # Filter ontwerp in R, daarvoor kun je de package control gebruiken.
   # Eventueel zou je ook nog kunnen kijken naar sysid, al betreft dat meer functies voor het interpreteren van step en frequency responses.
   signal <- as.matrix(series$y)
+  
+  #filter example 1 (simpel low pass filter)
+  n_samples <- 100
+  t <- seq(0, 1, len = n_samples)                                               # 1 second sample
+  t_s <- 1/n_samples
+  cutoff <- 10 #hz
+  filter_order <- 1
+  
+  bf <- butter(filter_order, t_s * cutoff, type = "low")                        # 10 Hz low-pass filter
+  x <- sin(2 * pi * t * 2.3) + 0.25 * rnorm(length(t))                          # 2.3 Hz sinusoid+noise
+  
+  
+  z <- filter(bf, x) # apply filter
+  u <- filtfilt(bf, x)
+  ggplotly(qplot(t, x, geom = "line") +
+    geom_line(aes(x=t, y=z), col = "red")+
+    geom_line(aes(x=t, y=u), col = "green") +
+      theme_light())
+
+  
+  freqz(bf)
   
   num <- c(1)
   den <- c(0.5, 1)
@@ -178,4 +198,88 @@ aki_hackathon_sta2 <- function(){
   ggplotly(prophet_plot_components(model, forecast)[[1]])
   
   return(1)
+}
+
+
+#' @title aki_hackathon_sta2
+#' @description Season trend analyse van profieldata2
+#' @import fpp3
+#' @import data.table
+#' @import slider
+#' @import spectral
+#' @import control
+#' @import plotly
+#' @import patchwork
+#' @import forecast
+#' @import MASS
+#' @import tseries
+#' 
+aki_hackathon_sta3 <- function(){
+  
+# https://github.com/MGCodesandStats/energy-weather-modelling/blob/master/arima-weather-dublin-airport.R
+  library(tsibble)
+  
+  trucje<- packageName()
+  if (is.null(trucje)) {trucje <- 'PROJECTNAME'}
+  
+  # profiel_data <- readRDS("/data/workspace/AKI_hackathon_sta/data_alkmaar.RDS")
+  profiel_data <- readRDS(system.file('rds/data_alkmaar.RDS', package = trucje))
+  
+  
+  ##voorbeeldset:
+  mydata<- read.csv("inst/extdata/mly532.csv")
+  attach(mydata)
+  summary(mydata)
+  meant <- ts(mydata$meant[1:732], start = c(1941,11), frequency = 12)
+  
+  
+  ##einde voorbeeld.
+  
+  
+  
+  # forecast_data <- unique(forecast_data#[year < 2022, .(ds, y)])
+  
+  profiel_data$installatie_profiel$DATUM_TIJD <- paste0(profiel_data$installatie_profiel$DATUM_TIJD, ":00")
+  forecast_data <- profiel_data$installatie_profiel[JAAR %in% c(2020:2025), .(ds = lubridate::as_datetime(DATUM_TIJD), y = BELASTING)]
+  schrikkeldag <-forecast_data[ds < as.Date('2020-02-23') & ds >= as.Date('2020-02-22')][, ds:= ds+ days(7)]
+  forecast_data <- rbind(forecast_data, schrikkeldag)
+  
+  #teken profiel
+  series <- tsibble(forecast_data, index=ds)
+  series %>%
+    autoplot(y, colour="gray") %>% ggplotly()
+  
+  # ACF, PACF and Dickey-Fuller Test
+  acf(meant, lag.max=20)
+  pacf(meant, lag.max=20)
+  adf.test(meant)
+  
+  components <- decompose(meant)
+  components
+  plot(components)
+  
+  # ARIMA
+  fitweatherarima<-auto.arima(meant, trace=TRUE, test="kpss", ic="bic")
+  fitweatherarima
+  confint(fitweatherarima)
+  
+  # Forecasted Values From ARIMA
+  forecastedvalues=forecast(fitweatherarima,h=183)
+  forecastedvalues
+  plot(forecastedvalues)
+  
+  # Test Values
+  test=mydata$meant[733:915]
+  test
+  
+  # Ljung-Box
+  Box.test(fitweatherarima$resid, lag=4, type="Ljung-Box")
+  Box.test(fitweatherarima$resid, lag=8, type="Ljung-Box")
+  Box.test(fitweatherarima$resid, lag=12, type="Ljung-Box")
+  
+  #RMSE
+  RMSE <- sqrt(mean((forecastedvalues$mean - test)^2))
+  RMSE
+  mean(test)
+
 }
